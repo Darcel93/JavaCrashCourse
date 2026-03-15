7b427b6d73d069176e46d549d2d5b8eaa3c6cc62
Darcel93 <darcel.sanders93@gmail.com>
2026-03-15 13:09:07 -0700

    NameGenertor with input

--- src/main/java/org/WuTangNameGenerator.java
+++ src/main/java/org/WuTangNameGenerator.java
@@ -1,18 +1,29 @@
 package org;
 
 import java.nio.file.SecureDirectoryStream;
+import java.util.Random;
+import java.util.Scanner;
 
 public class WuTangNameGenerator {
     public static void main(){
-        FirstNames();
-        SecondNames();
-        TitleName();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter First Name: ");
+        String FirstName = input.nextLine();
+        System.out.print("Enter Last Name: ");
+        String LastName = input.nextLine();
+        String NameFinal = FirstNames() + " " + SecondNames() + " " + TitleName();
+
+            System.out.print(NameFinal);
 
 
 
     }
 
+
     public static String FirstNames(){
+        Random generate = new Random();
+        int i = generate.nextInt(10);
+
         String[] Firstword = new String [11];
         Firstword[0] = "Crimson";
         Firstword[1] = "Shadow";
@@ -20,15 +31,20 @@ public class WuTangNameGenerator {
         Firstword[3] = "Storm";
         Firstword[4] = "Silent";
         Firstword[5] = "Ruthless";
-        Firstword[6] = "Frost";
+        Firstword[6] = "Draco";
         Firstword[7] = "Phantom";
         Firstword[8] = "Steel";
         Firstword[9] = "Nova";
         Firstword[10] = "Masta";
-        return Firstword[0];
+
+        return Firstword[i];
     }
 
     public static String SecondNames(){
+
+        Random generate = new Random();
+        int v = generate.nextInt(0, 10);
+
        String[] Secondword = new String[11];
        Secondword[0] = "Dragon";
        Secondword[1] = "Tiger";
@@ -41,9 +57,13 @@ public class WuTangNameGenerator {
        Secondword[8] = "Live";
        Secondword[9] = "Monarch";
        Secondword[10] = "God";
+
+       return Secondword[v];
     }
 
     public static String TitleName(){
+        Random generate = new Random();
+        int b = generate.nextInt(0, 10);
     String[] Titles = new String[11];
     Titles[0] = "The Chamber Guardian";
     Titles[1] = "The Beat Assassin";
@@ -56,6 +76,8 @@ public class WuTangNameGenerator {
     Titles[8] = "Strategist Supreme";
     Titles [9] = "The Silent Conqueror";
     Titles[10] = "The Elder God";
+
+    return Titles[b];
 
     }
 }
